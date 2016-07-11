7cef054d3d7068d654d3483acb38b5f3e7b8d0f6
asgalov <asgalov@gmail.com>
2016-07-11 17:20:19 +0300

    add animation template

--- IMU/rotation.py
+++ IMU/rotation.py
@@ -25,13 +25,6 @@ def plot_board(ax, b):
     ax.quiver(b.x, b.y, b.z, b.i[0], b.i[1], b.i[2], length=1,pivot="tail",color="r")
     ax.quiver(b.x, b.y, b.z, b.j[0], b.j[1], b.j[2], length=1,pivot="tail",color="b")
     ax.quiver(b.x, b.y, b.z, b.k[0], b.k[1], b.k[2], length=1,pivot="tail",color="g")
-    max_ax_val = 3
-    min_ax_val = -3
-    ax.set_xlim(min_ax_val, max_ax_val)
-    ax.set_ylim(min_ax_val, max_ax_val)
-    ax.set_zlim(min_ax_val, max_ax_val)
-    plt.show()
-
 
 q = Quaternion(1,1,1,1)
 q.counter = 123
@@ -41,4 +34,14 @@ print y.a, y.b
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 b = MIMUBoard()
+max_ax_val = 3
+min_ax_val = -3
+ax.set_xlim(min_ax_val, max_ax_val)
+ax.set_ylim(min_ax_val, max_ax_val)
+ax.set_zlim(min_ax_val, max_ax_val)
+num_of_frames = 25
+frame_interval_ms = 50
+array_of_function_parameters = ()
 plot_board(ax, b)
+#line_ani = animation.FuncAnimation(fig, plot_board, num_of_frames, fargs=(array_of_function_parameters), interval=frame_interval_ms, blit=False)
+plt.show()
