8025d14724bfb5f25dfba4ff14a72ddf0008c33f
Aleksandr Galov <asgalov@gmail.com>
2016-07-12 09:49:36 +0300

    animation of IMU board

--- IMU/mimu_animation.py
+++ IMU/mimu_animation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+import copy
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
@@ -7,14 +8,31 @@ import numpy as np
 
 fig = plt.figure()
 ax = fig.gca(projection='3d', xlim=(-3, 3), ylim=(-3, 3), zlim=(-3,3))
-v = (0,0,0,1,1,1) 
-
+ox, = ax.plot([], [], lw=2)
+oy, = ax.plot([], [], lw=2)
+oz, = ax.plot([], [], lw=2)
 
 def init_v():
-    return ax.quiver(v[0], v[1], v[2], v[3], v[4], v[5], length=1,pivot="tail",color="w")
+    ox.set_data([], [])
+    ox.set_3d_properties([])
+    oy.set_data([], [])
+    oy.set_3d_properties([])
+    oz.set_data([], [])
+    oz.set_3d_properties([])
+    return ox, oy, oz
 
 def plot_v(i):
-    return ax.quiver(v[0], v[1], v[2], v[3] + 2*i, v[4], v[5], length=1, pivot="tail", color="b")
+    x = 0
+    y = np.sin(2 * np.pi * (x - 0.01 * i))
+    z = np.sin(2 * np.pi * (x - 0.01 * i))
+    ox.set_data([x,x], [y,y+1])
+    ox.set_3d_properties([z,z])
+    oy.set_data([x,x+1], [y,y])
+    oy.set_3d_properties([z,z])
+    oz.set_data([x,x], [y,y])
+    oz.set_3d_properties([z,z+1])
+    return ox,oy,oz
 
-anim = animation.FuncAnimation(fig, plot_v, init_func=init_v, frames=20, interval=50, blit=True)
+anim = animation.FuncAnimation(fig, plot_v, init_func=init_v, frames=200, interval=10, blit=True)
+
 plt.show()
