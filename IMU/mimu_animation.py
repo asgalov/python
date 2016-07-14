44cb4bf6ea4f2835c237c08693a1b6379d51ece1
Aleksandr Galov <asgalov@gmail.com>
2016-07-14 15:36:47 +0300

    small refactoring

--- IMU/mimu_animation.py
+++ IMU/mimu_animation.py
@@ -1,29 +1,40 @@
 import numpy as np
-import pandas as pd
+import copy
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from mpl_toolkits.mplot3d import Axes3D
 from quaternion import Quaternion
 from mimu import Mimu
 
+# read IMU measurements in the following format:
+# 'n','t_sec', 'ax', 'ay', 'az', 'wx', 'wy', 'wz', 'mx', 'my', 'mz'
+msrlist = []
+with open('imu_sim_data.csv', 'r') as f:
+    keys = next(f).split()
+    for line in f:
+        values = map(float, line.split())
+        msrlist.append(dict(zip(keys, values)))
 
+# calc MIMU board attitude for each measurements
+mimu = Mimu()
+attitude = []
+for msr in msrlist:
+    alpha = msr['wy']
+    qz = Quaternion(np.cos(alpha / 2), 0, 0, np.sin(alpha / 2))
+    qx = Quaternion(np.cos(alpha / 2), np.sin(alpha / 2), 0, 0)
+    mimu.rotate(qz)
+    mimu.rotate(qx)
+    attitude.append(copy.deepcopy(mimu))
+ 
+# plow MIMU attitude
 fig = plt.figure()
 ax = fig.gca(projection='3d', xlim=(-3, 3), ylim=(-3, 3), zlim=(-3,3))
 ox, = ax.plot([], [], lw=2)
 oy, = ax.plot([], [], lw=2)
 oz, = ax.plot([], [], lw=2)
 
-mimu = Mimu()
-df = pd.read_csv('imu_sim_data.csv')
-
 def plot_v(n):
-    rnd = float(df.iloc[n]['wy']) * 0.1
-    print df.iloc[n]
-    alpha = np.pi * rnd / 10.0
-    qz = Quaternion(np.cos(alpha / 2), 0, 0, np.sin(alpha / 2))
-    qx = Quaternion(np.cos(alpha / 2), np.sin(alpha / 2), 0, 0)
-    mimu.rotate(qz)
-    mimu.rotate(qx)
+    mimu = attitude[n]
     plot_attitude(mimu)
     return ox,oy,oz
 
@@ -38,5 +49,4 @@ def plot_vector(ox, x0, y0, z0, q):
     ox.set_3d_properties([z0, z0 + q.d])
 
 anim = animation.FuncAnimation(fig, plot_v, interval=10, blit=True)
-
 plt.show()
