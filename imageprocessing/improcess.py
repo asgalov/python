8af755ba74a31678589f4c8b05a65adec77cbe2a
asgalov <asgalov@gmail.com>
2017-09-13 23:52:16 +0400

    remove bushes

--- imageprocessing/improcess.py
+++ imageprocessing/improcess.py
@@ -5,6 +5,7 @@ import scipy.ndimage as nd
 from skimage import filters
 from skimage import feature
 import matplotlib.pyplot as plt
+from skimage.morphology import watershed
 #from skimage.filter.thresholding import threshold_otsu
 
 
@@ -12,16 +13,27 @@ a = Image.open('resize.jpg').convert('L')
 a = mi.fromimage(a)
 
 thresh = filters.threshold_otsu(a)
-b = a > thresh
-b = mi.toimage(b)
-b.save('otsu_semoutput.png')
+im_otsu = a > thresh
+im_otsu = mi.toimage(im_otsu)
+im_otsu.save('otsu_semoutput.png')
 
 im_canny = feature.canny(a, sigma=3)
+fill_holes = nd.binary_fill_holes(im_canny)
+fill_holes = mi.toimage(fill_holes)
 im_canny = mi.toimage(im_canny)
 im_canny.save('test_canny.jpg')
+
+#to remove bushes try to adopt canny edges for leaves, fill holes and then negate obtained mask
+fill_holes.save('fill_holes.jpg')
 
 im_laplace = nd.gaussian_laplace(a, 3)
 im_laplace = mi.toimage(im_laplace)
 im_laplace.save('test_laplace.jpg')
 
-
+im_elevation_map = filters.sobel(a)
+markers = np.zeros_like(a)
+markers[a < 30] = 1
+markers[a > 150] = 2
+segmentation = watershed(im_elevation_map, markers)
+im_sobel = mi.toimage(segmentation)
+im_sobel.save('im_sobel.jpg')
