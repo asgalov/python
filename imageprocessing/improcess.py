5aae344f15d2543782066ce4831f30a01556cd52
asgalov <asgalov@gmail.com>
2017-09-13 23:22:51 +0400

    thershold segmentation

--- imageprocessing/improcess.py
+++ imageprocessing/improcess.py
@@ -5,10 +5,17 @@ import scipy.ndimage as nd
 from skimage import filters
 from skimage import feature
 import matplotlib.pyplot as plt
+#from skimage.filter.thresholding import threshold_otsu
 
 
 a = Image.open('resize.jpg').convert('L')
 a = mi.fromimage(a)
+
+thresh = filters.threshold_otsu(a)
+b = a > thresh
+b = mi.toimage(b)
+b.save('otsu_semoutput.png')
+
 im_canny = feature.canny(a, sigma=3)
 im_canny = mi.toimage(im_canny)
 im_canny.save('test_canny.jpg')
@@ -16,4 +23,5 @@ im_canny.save('test_canny.jpg')
 im_laplace = nd.gaussian_laplace(a, 3)
 im_laplace = mi.toimage(im_laplace)
 im_laplace.save('test_laplace.jpg')
+
 
